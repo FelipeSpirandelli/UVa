9b3e83801c1edbf1e19a15b82a4517f0f5769c4b
FelipeSpirandelli <felipereisspirandelli@gmail.com>
2021-04-17 18:55:38 -0300

    17/04/2021

---
+++ UVa 11850 - Alaska.cpp
@@ -0,0 +1,33 @@
+/*
+The only detail is that she has to come back, so there are two conditions 
+to be possible.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    int location, i;
+
+    while(scanf("%d", &n) && n){
+        vector<int> locations;
+        locations.clear();
+        int flag = 0;
+        while(n--){
+            scanf("%d", &location);
+            locations.push_back(location);
+        }
+        sort(locations.begin(),locations.end());
+        for(i=0;i<locations.size()-1;i++){
+            if(locations[i+1] - locations[i]>200) flag++;
+        }
+        if(flag == 0 && (locations[locations.size() - 1] + 100 >= 1422)) printf("POSSIBLE\n");
+        else printf("IMPOSSIBLE\n");
+    }
+
+    return 0;
+}
+
